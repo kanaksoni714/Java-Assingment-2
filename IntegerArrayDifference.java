7c9e948339eed46b0d3541f959e8b7b7c81835d5
kanaksoni714 <45351308+kanaksoni714@users.noreply.github.com>
2018-12-12 20:19:48 +0530

    Add files via upload

---
+++ IntegerArrayDifference.java
@@ -0,0 +1,24 @@
+public class IntegerArrayDifference {
+
+	public int findAbsoluteDifference(int[] numbers, int k) {
+
+		for(int i=0;i<numbers.length;i++){
+			
+			for(int j=i;j<numbers.length;j++){
+				
+				if(i!=j){
+					if(numbers[i]==numbers[j]){
+						int diff=Math.abs(i-j);
+						
+						return diff;
+					}
+
+
+				}
+				
+			}
+		}
+		return -1;
+	}
+
+}
