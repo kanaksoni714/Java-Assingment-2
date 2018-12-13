fd1468717a29f0e166841774c4d712563e426619
kanaksoni714 <45351308+kanaksoni714@users.noreply.github.com>
2018-12-13 17:15:28 +0530

    Add files via upload

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,20 @@
+public class MultidimensionalArray {
+
+
+	public boolean searchElement(int numberToBeSearch, int[][] numbers) {
+
+		for(int row=0;row<numbers.length;row++){
+			
+			for(int column=0;column<numbers[0].length;column++){
+				
+				if(numberToBeSearch==numbers[row][column]){
+					return true;
+				}
+			}
+			
+		}
+		
+		return false;
+	}
+
+}
